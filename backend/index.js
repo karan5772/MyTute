86f0b1adec568c1f3bebefba97cf051eaa5ec29a
karan5772 <karankumar8239@gmail.com>
2026-07-03 02:44:04 +0530

    ....

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true, limit: "1000mb" }));
 //remove on VPS, nginx got it
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://mytute.dsasnippets.xyz"],
+    origin: ["http://localhost:5173", "https://my-tute-mocha.vercel.app/"],
     credentials: true,
   }),
 );
